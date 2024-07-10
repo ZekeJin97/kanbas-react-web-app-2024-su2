ccb133a5df9eb223615601651a505a3ba2dfd8df
ZekeJin97 <97zjin@gmail.com>
2024-07-09 19:10:46 -0700

    A1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,12 +7,13 @@ export default function Labs() {
     return (
         <div>
             <h1>Labs</h1>
-            <TOC />
+            <h1>Zhechao Jin</h1>
+            <TOC/>
             <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1 />} />
-                <Route path="Lab2" element={<Lab2 />} />
-                <Route path="Lab3" element={<Lab3 />} />
+                <Route path="/" element={<Navigate to="Lab1"/>}/>
+                <Route path="Lab1" element={<Lab1/>}/>
+                <Route path="Lab2" element={<Lab2/>}/>
+                <Route path="Lab3" element={<Lab3/>}/>
             </Routes>
         </div>
     );
